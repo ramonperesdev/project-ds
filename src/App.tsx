c8f3147718e4b896a945f16094f9d541b814e814
Ramon Nobre Peres <r.peres@xsensors.ai>
2022-10-14 00:18:56 -0300

    implemented layout of sign in

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Heading } from "./components/Heading";
 import { Text } from "./components/Text";
 import { TextInput } from "./components/TextInput";
 import { Logo } from "./Logo";
+
 import "./styles/global.css";
 
 export function App() {
